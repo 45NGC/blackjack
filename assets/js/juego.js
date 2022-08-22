b681dd6f24e1fc5a9af9aa6f2f79803c2e0bf803
45NGC <nicolasnerfo@gmail.com>
2022-08-22 10:28:50 +0200

    Añadida funcion pedirCarta (video 50)

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -12,6 +12,7 @@ let palos = 			['C','D','H','S']; // CLUBS, DIAMONDS, HEARTS, SPADES
 let tiposNoNumericos = 	['J','Q','K','A']; // JACK (11), QUEEN (12), KING (13), ASE (1)
 
 const crearBaraja = () => {
+	// Esta funcion devuelve una baraja (en forma de array) totalmente desordenada
 
 	// Añadimos las cartas numericas de la baraja (del 2 al 10)
 	for(let i = 2 ; i <= 10; i++) {
@@ -27,14 +28,28 @@ const crearBaraja = () => {
 		}
 	}
 
-	//console.log(`Baraja ordenada : ${ baraja }`);
+	console.log(`Baraja ordenada : ${ baraja }`);
 
 	// Barajamos nuestra baraja :
 	baraja = _.shuffle( baraja );
 
-	//console.log(`Baraja barajada : ${ baraja }`);
+	console.log(`Baraja desordenada : ${ baraja }`);
 
 	return baraja;
 }
 
+const pedirCarta = () => {
+	// Esta funcion nos permite tomar una carta de la baraja
+	
+	// En caso de que no tengamos cartas dentro de nuestra baraja mostramos un error
+	if(baraja.length === 0){
+		throw 'NO HAY MAS CARTAS DENTRO DE LA BARAJA';
+	}
+	const carta = baraja.pop();
+	console.log(`Carta tomada : ${ carta }`)
+	return carta;
+}
+
 crearBaraja();
+pedirCarta();
+console.log(`Baraja sin una carta : ${ baraja }`);
